893bc9598f757badd3a31b4cff032fdca85cf7cd
vini3434 <119761931+vini3434@users.noreply.github.com>
2025-04-14 20:01:58 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Student implements Serializable {
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public void displayDetails() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+        System.out.println("Student GPA: " + gpa);
+    }
+}
